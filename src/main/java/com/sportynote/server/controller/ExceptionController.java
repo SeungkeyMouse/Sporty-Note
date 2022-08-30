f2f0940dab7334b652280b4c6ace5460c2a5d485
yeongha <fourjae@naver.com>
2022-08-30 21:25:51 +0900

    [backup]

--- src/main/java/com/sportynote/server/controller/ExceptionController.java
+++ src/main/java/com/sportynote/server/controller/ExceptionController.java
@@ -1,27 +1,31 @@
 package com.sportynote.server.controller;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import lombok.Getter;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.NoSuchElementException;
 
 /**
  * 예외 처리용 컨트롤러 입니다.
  * */
-@RestControllerAdvice
+//@RestControllerAdvice
+@RestController
 public class ExceptionController {
 
-    @ExceptionHandler(NoSuchElementException.class)
-    @ResponseBody
-    public String noItemErrorHandler(NoSuchElementException e){
-        return "Item Not Exists!";
+//    @ExceptionHandler(NoSuchElementException.class)
+//    @ResponseBody
+//    public String noItemErrorHandler(NoSuchElementException e){
+//        return "Item Not Exists!";
+//    }
+//
+//    @ExceptionHandler(NumberFormatException.class)
+//    @ResponseBody
+//    public String noItemErrorHandler(NumberFormatException e){
+//        return "Invalid Path Variable!";
+//    }
+    @GetMapping("/12345")
+    public String Test() {
+        return "Test";
     }
-
-    @ExceptionHandler(NumberFormatException.class)
-    @ResponseBody
-    public String noItemErrorHandler(NumberFormatException e){
-        return "Invalid Path Variable!";
-    }
-
 }
+
