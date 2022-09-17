c035ca0c7b2569e9a9c91b176f9ad2700b4e0fea
yeongha <fourjae@naver.com>
2022-09-17 18:01:20 +0900

    [update] jasypt setting and properties enc

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM adoptopenjdk/openjdk11
 CMD ["./mvnw", "clean", "package"]
 ARG JAR_FILE_PATH=build/libs/*.jar
 COPY ${JAR_FILE_PATH} server-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java", "-jar", "server-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "server-0.0.3-SNAPSHOT.jar"]
--- src/test/java/com/sportynote/server/ServerApplicationTests.java
+++ src/test/java/com/sportynote/server/ServerApplicationTests.java
@@ -1,6 +1,9 @@
 package com.sportynote.server;
 
+import com.sportynote.server.config.JasyptConfig;
 import com.sportynote.server.mysql.MySQLConnectionTest;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,29 +11,33 @@ import org.springframework.context.annotation.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Configuration
 @SpringBootTest(classes= MySQLConnectionTest.class)
 class ServerApplicationTests {
-	@Value(value = "${spring.datasource.driver-class-TEST}")
-	private String DRIVER;
 
-	@Value(value ="${spring.datasource.url}")
-	private String URL ;
-
-	@Value(value ="${spring.datasource.username}")
-	private String USER ;
-
-	@Value(value ="${spring.datasource.password}")
-	private String PASSWORD ;
 
 	@Test
-	void contextLoads() throws Exception{
-			System.out.println(DRIVER);
-			Class.forName(DRIVER);
-			try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-				System.out.println(connection);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+	void contextLoads() throws Exception {
+		String plainText = "hello";
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword("password");
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
+		config.setProviderName("SunJCE");
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+
+		String encryptText = encryptor.encrypt(plainText);
+		System.out.println(encryptText);
+		String decryptText = encryptor.decrypt(encryptText);
+		System.out.println(decryptText);
+		assertThat(plainText).isEqualTo(decryptText);
 	}
+}
