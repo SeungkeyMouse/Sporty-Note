9d1e9b22bfe23690c225d4116b772833ae83d0d7
yeongha <fourjae@naver.com>
2022-09-18 15:26:34 +0900

    [update] env

--- src/main/java/com/sportynote/server/config/JasyptConfig.java
+++ src/main/java/com/sportynote/server/config/JasyptConfig.java
@@ -3,18 +3,27 @@ package com.sportynote.server.config;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String password; // 암호화 시 사용될 키 값
+    @Autowired
+    ApplicationContext context;
+//    @Value("${jasypt.encryptor.password}")
+//    private String password; // 암호화 시 사용될 키 값
 
     @Bean("jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
+        Environment env = context.getEnvironment();
+        String password=env.getProperty("jasypt.encryptor.password");
+        System.out.println(password);
+
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
         config.setPassword(password); // 암호화 키 값
